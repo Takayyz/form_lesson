1fc4c84872a122b04cf3a52301d69683688feb37
t_hino <hino@gizumo-inc.jp>
2020-11-25 16:51:51 +0900

    add: thanksページ

---
+++ thanks.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>登録完了画面</title>
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+  </head>
+  <body>
+    <section class="wrapper">
+      <div class="inner">
+        <div class="main-content">
+          <h1 class="form-title">登録完了</h1>
+          <p>さん、登録ありがとうございました。</p>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
